d978cdff311b42dde558a6b7ccc25a751a0134fc
Isabel VM <isa_vargasmorales@hotmail.com>
2020-05-14 13:39:02 -0500

    Agregación de base de datos local, conexión con Entity, creación de la clase BusinessCitas

---
+++ SolOrogolden/WebOrogolden/Business/BusinessCitas.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services.Description;
+using WebOrogolden.Models;
+
+namespace WebOrogolden.Business
+{
+    public class BusinessCitas
+    {
+        public void AddCita(Citas citas)
+        {
+            using (Database1Entities db = new Database1Entities())
+            {
+                Citas emai = db.Citas.Where(x => x.Email == citas.Email).FirstOrDefault();
+                if (emai == null)
+                {
+                    db.Citas.Add(citas);
+                    db.SaveChanges();
+                }
+                else
+                {
+                    throw new ApplicationException("Usted ya tiene una cita reservada");
+                }
+            }
+        }
+    }
+}
